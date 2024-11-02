a7bd40513f75768ee395c0e134f1e48274c4859c
otherenergy <otherenergy@hotmail.com>
2024-11-02 20:55:49 +0100

    Mantenimiento

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 export default function Page() {
-  return <h1>¡Estamos en Mantenimiento!</h1>
-    <div>
-        <p>Estamos en Mantenimiento, Disculpa las molestias, pero estamos realizando tareas de mantenimiento en este momento. Si lo necesitas, siempre puedes  <a href="mailto:#">contactarnos</a>, de lo contrario, ¡volveremos a estar en línea en breve!</p>
-        <p>&mdash; Pasionsex.com </p>
-    </div>;
+  return <h1> Estamos en Mantenimiento!
+  <p>Disculpa las molestias pero estamos realizando tareas de mantenimiento en este momento. Si lo necesitas siempre puedes <a href="mailto:#">contactarnos</a>, de lo contrario ¡volveremos a estar en línea en breve!</p>
+  <p> Pasionsex.com </p></h1>;
 }
