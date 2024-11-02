d045ecca31cf3263b2d7d112c439c997554cfca6
otherenergy <otherenergy@hotmail.com>
2024-11-02 18:15:15 +0100

    Nuevo directorio

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return <h1>¡Estamos en Mantenimiento!</h1>
+    <div>
+        <p>Estamos en Mantenimiento, Disculpa las molestias, pero estamos realizando tareas de mantenimiento en este momento. Si lo necesitas, siempre puedes  <a href="mailto:#">contactarnos</a>, de lo contrario, ¡volveremos a estar en línea en breve!</p>
+        <p>&mdash; Pasionsex.com </p>
+    </div>;
+}
