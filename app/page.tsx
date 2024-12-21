b1767b1f1636a21abdaf6b3d4496f646a3ed290a
otherenergy <39650427+otherenergy@users.noreply.github.com>
2024-12-21 12:17:33 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
   return <h1> Estamos en Mantenimiento!
-  <p>Disculpa las molestias pero estamos realizando tareas de mantenimiento en este momento. Si lo necesitas siempre puedes <a href="mailto:admin@diatradaou.com">contactarnos al correo:  admin@pasionsex.com</a>, de lo contrario ¡volveremos a estar en línea en breve!</p>
+  <p>Disculpa las molestias pero estamos realizando tareas de mantenimiento en este momento. Si lo necesitas siempre puedes <a href="mailto:admin@diatradaou.com">contactarnos al correo:  admin@diatradaou.com</a>, de lo contrario ¡volveremos a estar en línea en breve!</p>
   <p> diatradaou.com </p></h1>;
 }
